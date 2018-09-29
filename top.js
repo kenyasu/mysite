d9996985dbe17200147ed7aaf677e91e7d7f5389
kenyasu <kenyasukoharu@yahoo.co.jp>
2018-09-30 04:46:50 +0900

    C:/Program Files/Git/レスポンシブまで

--- top.js
+++ top.js
@@ -1,12 +1,14 @@
 $(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 500) {
-      $('#sticky-header').show();
-      $('#sticky-bar').addClass('is-fixed').slideDown();
-    } else {
-      $('#sticky-bar').slideUp();
-    }
-  });
+  if(window.matchMedia('(min-width: 1025px)').matches){
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+        $('#sticky-header').show();
+        $('#sticky-bar').addClass('is-fixed').slideDown();
+      } else {
+        $('#sticky-bar').slideUp();
+      }
+    });
+  }
 
   var pagetop = $('#back-icon');
     $(window).scroll(function(){
@@ -20,4 +22,11 @@ $(function () {
       $('body, html').animate({scrollTop:0}, 500);
         return false;
     });
+
+  $('.nav-content .suki-side > a').hover(
+    function(){
+      $(this).next('ul').slideToggle();
+    }, function(){
+      $(this).next('ul').slideUp();
+    })
 });
